f859ceee5610c7fc141fba5e6114b7929015dc9b
Nikita <mukuta95@gmail.com>
2019-10-25 13:26:39 +0300

    Push try 4

--- dao/data.py
+++ dao/data.py
@@ -3,4 +3,4 @@ password = 'StepWolf13'
 host = 'localhost'
 port = '5432'
 database = 'postgres'
-DATABASE_URI = 'postgres+psycopg2://postgres:{}@{}:{}/{}'.format(password, host, port, database)
+DATABASE_URI = 'postgres://ilcznheklixrea:83302c3c307466ef840777f073a6d1cf5f05e40867f9d52e9c9852121375c145@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/d8n0ok7liadetb'
